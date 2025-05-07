df9f82f78e7fa4bc1445ac232aea3d448ba4a434
Hubblle <lubin.tsch@gmail.com>
2025-05-07 17:40:23 +0200

    Refactor to let the user give yokai from his inventory

--- script/main.py
+++ script/main.py
@@ -1037,16 +1037,21 @@ class Trade(commands.Cog):
     
     
     @commands.hybrid_command(name="trade")
-    async def trade(self, ctx, ton_yokai : str, destinataire : discord.User, son_yokai : str):
+    async def trade(self, ctx, ton_yokai : str, destinataire : discord.User, son_yokai : str = ""):
         
         """
         
-        Cette commande permet d'échanger des Yo-kai entre deux utilisateurs. Elle s'utilise ainsi :
-        `/trade {Le Yo-kai que vous proposez}  {L'utilisateur avec qui vous voulez l'échanger}  {Le Yo-kai que vous voulez en retour}.`
+        **Cette commande permet d'échanger des Yo-kai entre deux utilisateurs. Elle s'utilise ainsi :**
+        `/trade {Le Yo-kai que vous proposez}  {L'utilisateur avec qui vous voulez l'échanger}  {Le Yo-kai que vous voulez en retour}`
 
-        -L'utilisateur qui exécute la commande peut annuler l'échange avant qu'il soit accepté en utilisant la réaction ❌.
-        -L'utilisateur qui reçoit la proposition peut la refuser avec ❌ ou l'accepter avec ✅. Il dispose de 30 secondes avant que la demande soit annulée.
-        -Vous pouvez trade plusieurs Yo-kai en les séparent par une virgule ; `/trade {Onisoi, Potache} {utilisateur} {Darabajoie, Espi}`
+        - L'utilisateur qui exécute la commande peut annuler l'échange avant qu'il soit accepté en utilisant la réaction ❌.
+        - L'utilisateur qui reçoit la proposition peut la refuser avec ❌ ou l'accepter avec ✅. Il dispose de 30 secondes avant que la demande soit annulée.
+        - Vous pouvez trade plusieurs Yo-kai en les séparent par une virgule ; `/trade {Onisoi, Potache} {utilisateur} {Darabajoie, Espi}`
+        
+        **Si vous utilisez la commande sans spécifier de Yo-kai en échange :**
+        `/trade {Le Yo-kai que vous proposez} {L'utilisateur avec qui vous voulez l'échanger}`
+        Cela vous permet de donner un Yo-kai. Vous pouvez aussi en mettre plusieurs, mais la limite est de **2**.
+
         
         -# Note :
         -# L'équipe du support n'est en aucun cas responsable si vous échangez un Yo-kai par erreur. Aucun Yo-kai ne sera remboursé.
@@ -1074,13 +1079,30 @@ class Trade(commands.Cog):
             recipient_inv = await get_inv(destinataire.id)
             
             #Format the yokai(s) in a tuple with separator ","
-            son_yokai = tuple(son_yokai.split(sep=", "))
+            if not son_yokai == "":
+                son_yokai = tuple(son_yokai.split(sep=", "))
             ton_yokai = tuple(ton_yokai.split(sep=", "))
+            
             
             
             #Do they have the yokai :
             author_have_it = False
             recipient_have_it = False
+            give = False
+            
+            #First of all, is the command used to give ?
+            if son_yokai == "" :
+                give = True
+                recipient_have_it = True
+            
+            #check if the give limit is exceed
+            if give == True :
+                if len(ton_yokai) > 2 :
+                    error_embed = discord.Embed(color=discord.Color.red(),
+                                        title="Vous ne pouvez pas donner plus de 2 Yo-kai en même temps ! 😒 (On est pas trop généreux quand même.)",
+                                        description="Merci de demander un Yo-kai en retour pour se faire."
+                                        )
+                    return await ctx.send(embed = error_embed)
             
             #check for the author
             corect_yokai_number_author = 0
@@ -1094,14 +1116,22 @@ class Trade(commands.Cog):
                         
             
             #check for the recipient
-            corect_yokai_number_recipient = 0
-            if not recipient_inv == {} :
-                for asked_yokai in son_yokai :
-                    for yokai in recipient_inv :
-                        if yokai == asked_yokai:
-                            corect_yokai_number_recipient += 1
-                if len(son_yokai) == corect_yokai_number_recipient :
-                    recipient_have_it = True
+            if give == False :
+                corect_yokai_number_recipient = 0
+                if not recipient_inv == {} :
+                    for asked_yokai in son_yokai :
+                        for yokai in recipient_inv :
+                            if yokai == asked_yokai:
+                                corect_yokai_number_recipient += 1
+                    if len(son_yokai) == corect_yokai_number_recipient :
+                        recipient_have_it = True
+            else :
+                if recipient_inv == {} :
+                    error_embed = discord.Embed(color=discord.Color.red(),
+                                        title=f"Le Medallium de {destinataire.name} est vide !",
+                                        description="On va dire que j'avais la flemme de gérer ce cas, dcp dites lui de faire /yokai et relancez la commande svp."
+                                        )
+                    return await ctx.send(embed = error_embed)
 
             #if one of them don't have it 
             if not author_have_it:
@@ -1109,53 +1139,85 @@ class Trade(commands.Cog):
             if not recipient_have_it :
                 return await ctx.send(embed=dont_have_it_embed("r"))
             
-            #format the asked yokai :            
-            asked_yokai_form = ""
-            for asked_yokai in son_yokai :
-                asked_yokai_form += f"> Le Yo-kai **{asked_yokai}** de rang **{classid_to_class(recipient_inv[asked_yokai][0])}**\n "
+            #format the asked yokai :
+            if give == False :        
+                asked_yokai_form = ""
+                for asked_yokai in son_yokai :
+                    asked_yokai_form += f"> Le Yo-kai **{asked_yokai}** de rang **{classid_to_class(recipient_inv[asked_yokai][0])}**\n "
             
             offered_yokai = ""
             for asked_yokai in ton_yokai :
                 offered_yokai += f"> Le Yo-kai **{asked_yokai}** de rang **{classid_to_class(author_inv[asked_yokai][0])}**\n "
             
             
-            
-            ask_embed = discord.Embed(color=discord.Color.green(),
-                                      title=f"Demande de trade entre {ctx.author.display_name} et {destinataire.display_name} !",
-                                      description="Merci de réagir avec ✅ pour accepter, ou ❌ pour annuler.\n **Vous pouvez voir les details du trade ci dessous.** \n -----------------------------------------------------"
-                                      )
-            ask_embed.add_field(name=f"Offre de {ctx.author.display_name} 🔀:",
-                                value=offered_yokai,
-                                inline=False
-                                )
-            ask_embed.add_field(name="Contre 🔀:",
-                                value=asked_yokai_form,
-                                inline=False
-                                )
-            ask_embed.set_author(name="🕰️ La demande timeout au bout de 30s.")
+            if give == False :
+                ask_embed = discord.Embed(color=discord.Color.green(),
+                                        title=f"Demande de trade entre {ctx.author.display_name} et {destinataire.display_name} !",
+                                        description="Merci de réagir avec ✅ pour accepter, ou ❌ pour annuler.\n **Vous pouvez voir les details du trade ci dessous.** \n -----------------------------------------------------"
+                                        )
+                ask_embed.add_field(name=f"Offre de {ctx.author.display_name} 🔀:",
+                                    value=offered_yokai,
+                                    inline=False
+                                    )
+                ask_embed.add_field(name="Contre 🔀:",
+                                    value=asked_yokai_form,
+                                    inline=False
+                                    )
+                ask_embed.set_author(name="🕰️ La demande timeout au bout de 1min.")
+                
+            else :
+                ask_embed = discord.Embed(color=discord.Color.green(),
+                                        title=f"{ctx.author.display_name} Fait un cadeau à {destinataire.display_name} !",
+                                        description="Merci de réagir avec ✅ pour confirmez, ou ❌ pour annuler.\n **Vous pouvez voir les details ci dessous.** \n -----------------------------------------------------"
+                                        )
+                ask_embed.add_field(name=f"Offre de {ctx.author.display_name} 🔀:",
+                                    value=offered_yokai,
+                                    inline=False
+                                    )
+                ask_embed.set_author(name="🕰️ L'offre timeout au bout de 1min.")
             
             #Send the message to ask
             ask_message = await ctx.send(embed=ask_embed)
             await ask_message.add_reaction("✅")
             await ask_message.add_reaction("❌")
-            bot_logger.info(f"{ctx.author.name} a demandé un trade à {destinataire.name}, il demande {son_yokai} contre {ton_yokai}, dans {ctx.guild.name}")
-            
+            if give == False :
+                bot_logger.info(f"{ctx.author.name} a demandé un trade à {destinataire.name}, il demande {son_yokai} contre {ton_yokai}, dans {ctx.guild.name}")
+            else :
+                bot_logger.info(f"{ctx.author.name} a fait un cadeau à {destinataire.name}, il offre {ton_yokai}, dans {ctx.guild.name}")
+                
             #How to check for the reaction
-            def reaction_check(reaction, user):
-                return ask_message.id == reaction.message.id and (ctx.author == user and str(reaction.emoji) == "❌") or (destinataire == user and (str(reaction.emoji) == "✅" or str(reaction.emoji) == "❌"))
-            
+            if give == False:
+                def reaction_check(reaction, user):
+                    return ask_message.id == reaction.message.id and (ctx.author == user and str(reaction.emoji) == "❌") or (destinataire == user and (str(reaction.emoji) == "✅" or str(reaction.emoji) == "❌"))
+                
+            else :
+                def reaction_check(reaction, user):
+                    return ask_message.id == reaction.message.id and ctx.author == user and (str(reaction.emoji) == "✅" or str(reaction.emoji) == "❌")
+                
+                
+                
             try :
                 #wait fot reaction
-                reaction, reaction_user = await bot.wait_for("reaction_add", timeout= 30, check=reaction_check)
+                reaction, reaction_user = await bot.wait_for("reaction_add", timeout= 60, check=reaction_check)
             except TimeoutError:
                 #what if no reaction after 30s
-                denied_embed = discord.Embed(color=discord.Color.red(), title="🛑 Votre demande de trade a été annulée car aucune activité durant 30s.", description="Merci de relancer la commande")
+                if give == False :
+                    denied_embed = discord.Embed(color=discord.Color.red(), title="🛑 Votre demande de trade a été annulée car aucune activité durant 1min.", description="Merci de relancer la commande")
+                else:
+                    denied_embed = discord.Embed(color=discord.Color.red(), title="🛑 Votre offre a été annulée car aucune activité durant 1min.", description="Merci de relancer la commande")
+               
                 return await ctx.send(embed=denied_embed)
             
             #What if the author cancel the request
             if reaction_user.id == ctx.author.id and reaction.emoji == "❌":
-                denied_embed = discord.Embed(color=discord.Color.red(), title=" 🛑 Votre demande de trade a été annulée", description="Merci de relancer la commande si cela était une erreur.")
-                bot_logger.info(f"{ctx.author.name} a annulée sa demande de trade avec {destinataire.name}, dans {ctx.guild.name}")
+                if give == False:
+                    denied_embed = discord.Embed(color=discord.Color.red(), title=" 🛑 Votre demande de trade a été annulée", description="Merci de relancer la commande si cela était une erreur.")
+                    bot_logger.info(f"{ctx.author.name} a annulée sa demande de trade avec {destinataire.name}, dans {ctx.guild.name}")
+                
+                else :
+                    denied_embed = discord.Embed(color=discord.Color.red(), title=" 🛑 Votre offre a été annulée", description="Merci de relancer la commande si cela était une erreur.")
+                    bot_logger.info(f"{ctx.author.name} a annulée son cadeau pour {destinataire.name}, dans {ctx.guild.name}")
+                
                 return await ctx.send(embed=denied_embed)
             
             
@@ -1169,18 +1231,19 @@ class Trade(commands.Cog):
             
             #ADD the yokais in the invs
             #ADD to the author inv and +1 to the class count
-            for asked_yokai in son_yokai:
-                try:
-                    author_inv[asked_yokai]
+            if give == False :
+                for asked_yokai in son_yokai:
                     try:
-                        #stack the yokai
-                        author_inv[asked_yokai][1] += 1
-                    except :
-                        author_inv[asked_yokai].append(2)
-                except KeyError:
-                    author_inv[asked_yokai] = [recipient_inv[asked_yokai][0]]
-                    author_inv[author_inv[asked_yokai][0]] += 1
-                
+                        author_inv[asked_yokai]
+                        try:
+                            #stack the yokai
+                            author_inv[asked_yokai][1] += 1
+                        except :
+                            author_inv[asked_yokai].append(2)
+                    except KeyError:
+                        author_inv[asked_yokai] = [recipient_inv[asked_yokai][0]]
+                        author_inv[author_inv[asked_yokai][0]] += 1
+                    
             #ADD to the recipient inv
             for asked_yokai in ton_yokai :
                 try :
@@ -1218,37 +1281,49 @@ class Trade(commands.Cog):
                 
              
             # FIRST ; do they have more than one of this yokai ?
-            for asked_yokai in son_yokai :   
-                try :
-                    one_more_recipient = recipient_inv[asked_yokai][1] > 1
-                except :
-                    one_more_recipient = False
-                
+            if give == False :
+                for asked_yokai in son_yokai :   
+                    try :
+                        one_more_recipient = recipient_inv[asked_yokai][1] > 1
+                    except :
+                        one_more_recipient = False
                     
-                if one_more_recipient == True :
-                    #just remove the mention of several yokai if there are juste two
-                    if recipient_inv[asked_yokai][1] == 2:
-                        recipient_inv[asked_yokai].remove(recipient_inv[asked_yokai][1])
-                    else:
-                        recipient_inv[asked_yokai][1] -= 1
                         
-                else :
-                    recipient_inv.pop(asked_yokai)
-                    recipient_inv[author_inv[asked_yokai][0]] -= 1
+                    if one_more_recipient == True :
+                        #just remove the mention of several yokai if there are juste two
+                        if recipient_inv[asked_yokai][1] == 2:
+                            recipient_inv[asked_yokai].remove(recipient_inv[asked_yokai][1])
+                        else:
+                            recipient_inv[asked_yokai][1] -= 1
+                            
+                    else :
+                        recipient_inv.pop(asked_yokai)
+                        recipient_inv[author_inv[asked_yokai][0]] -= 1
                     
             #Save the inv
             await save_inv(author_inv, ctx.author.id)
             await save_inv(recipient_inv, destinataire.id)
                 
             #send the success embed
-            success_embed = discord.Embed(colour=discord.Color.green(),
-                                          title="__**Le trade a été effectué**__ ✅",
-                                          description="> Ci dessous vous pouvez vois le bilan du trade."
-                                          )
-            success_embed.add_field(name=f"{ctx.author.name} a eu le(s) Yo-kai :", value=asked_yokai_form, inline=False)
-            success_embed.add_field(name=f"{destinataire.name} a eu le(s) Yo-kai :", value=offered_yokai, inline=False)
+            if give == False :
+                success_embed = discord.Embed(colour=discord.Color.green(),
+                                            title="__**Le trade a été effectué**__ ✅",
+                                            description="> Ci dessous vous pouvez vois le bilan du trade."
+                                            )
+                success_embed.add_field(name=f"{ctx.author.name} a eu le(s) Yo-kai :", value=asked_yokai_form, inline=False)
+                success_embed.add_field(name=f"{destinataire.name} a eu le(s) Yo-kai :", value=offered_yokai, inline=False)
+                
+                bot_logger.info(f"{destinataire.name} a accepté le trade de {ctx.author.name}, il demandait {son_yokai} contre {ton_yokai}, dans {ctx.guild.name}")
             
-            bot_logger.info(f"{destinataire.name} a accepté le trade de {ctx.author.name}, il demandait {son_yokai} contre {ton_yokai}, dans {ctx.guild.name}")
+            else :
+                success_embed = discord.Embed(colour=discord.Color.green(),
+                                            title="__**Le cadeau a bien été envoyé !**__ ✅",
+                                            description="> Ci dessous vous pouvez vois le bilan."
+                                            )
+                success_embed.add_field(name=f"{destinataire.name} a eu le(s) Yo-kai :", value=offered_yokai, inline=False)
+                bot_logger.info(f"{ctx.author.name} a offert {ton_yokai} à {destinataire.name}, dans {ctx.guild.name}")
+                
+            
             return await ctx.send(embed=success_embed)
             
             
